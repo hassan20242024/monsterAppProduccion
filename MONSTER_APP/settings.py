806ef6123034b340cc04680f44e21545ef7a69a3
hassan20242024 <hassan_84@hotmail.es>
2025-08-15 16:20:43 -0500

    Para fines de producción

--- MONSTER_APP/settings.py
+++ MONSTER_APP/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5o95fv@r=-mat=@24fba40ptmt5z%51(-y^$am_-dtl**#73z#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ["139.177.204.185"]
 ALLOWED_HOSTS = ["*"] #paraentorno de desarrollo
